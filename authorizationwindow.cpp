30f077f4fcd1299e26ca18a89683dbd646bc7672
aneroid11 <a-e-v2003@yandex.by>
2022-03-19 21:04:24 +0300

    Entering login and password

--- authorizationwindow.cpp
+++ authorizationwindow.cpp
@@ -1,20 +1,35 @@
 #include <QPushButton>
 #include <QGridLayout>
+#include <QLineEdit>
 
 #include "authorizationwindow.h"
 
 AuthorizationWindow::AuthorizationWindow(QWidget *parent) : QWidget(parent)
 {
-    setFixedSize(size());
+    setFixedWidth(400);
+    setFixedHeight(200);
 
-    QPushButton* example_button = new QPushButton("Кнопка", this);
-    QPushButton* back_button = new QPushButton("Назад", this);
+    // сверху два поля «логин» и «пароль», снизу кнопки: «войти», «зарегистрироваться», «назад к выбору банка».
 
-    connect(back_button, &QPushButton::pressed, this, &AuthorizationWindow::back);
+    QLineEdit* loginLine = new QLineEdit(this);
+    loginLine->setPlaceholderText("Логин");
+
+    QLineEdit* passwordLine = new QLineEdit(this);
+    passwordLine->setPlaceholderText("Пароль");
+    passwordLine->setEchoMode(QLineEdit::EchoMode::Password);
+
+    QPushButton* enterButton = new QPushButton("Войти", this);
+    QPushButton* signupButton = new QPushButton("Зарегистрироваться", this);
+    QPushButton* backButton = new QPushButton("Назад к выбору банка", this);
+
+    connect(backButton, &QPushButton::pressed, this, &AuthorizationWindow::back);
 
     QGridLayout* grid = new QGridLayout(this);
-    grid->addWidget(example_button, 0, 0);
-    grid->addWidget(back_button, 1, 0);
+    grid->addWidget(loginLine, 0, 0);
+    grid->addWidget(passwordLine, 1, 0);
+    grid->addWidget(enterButton, 2, 0);
+    grid->addWidget(signupButton, 3, 0);
+    grid->addWidget(backButton, 4, 0);
 }
 
 void AuthorizationWindow::back()
