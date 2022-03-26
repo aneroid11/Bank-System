7bb8963af5a65406314af0b92b971e49f57586ea
aneroid11 <a-e-v2003@yandex.by>
2022-03-26 17:43:29 +0300

    first registrated user

--- banksystemmodel.cpp
+++ banksystemmodel.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 
 #include "banksystemmodel.h"
+#include "user.h"
 #include "hashcomputer.h"
 #include "database.h"
 #include "dbnotopenedexception.h"
@@ -48,6 +49,19 @@ void BankSystemModel::enter(std::string login, std::string password)
 void BankSystemModel::sendSignupRequestForClient(std::string login, std::string password, std::string email,
                                                  std::string name, std::string phone)
 {
+    // Создаём пользователя. Пока просто создаём и добавляем в базу. Потом сделаем там поле approved,
+    // которое будет показывать, подтвердил ли менеджер юзера.
 
-    //std::cout << name << ": юзер отправил запрос на регистрацию\n";
+    User::Data data =
+    {
+        database->generateUniqueUserId(),
+        name,
+        phone,
+        email,
+        login,
+        HashComputer().hash(password)
+    };
+
+    User newUser(data);
+    database->addUser(newUser);
 }
