58cd256ab15bf8581b2be5b7a451e7e2df2f96cf
aneroid11 <a-e-v2003@yandex.by>
2022-04-16 20:46:16 +0300

    dummy credits window

--- clientcreditswindow.cpp
+++ clientcreditswindow.cpp
@@ -1,6 +1,18 @@
+#include <QDebug>
+#include <QPushButton>
+#include <QGridLayout>
+
 #include "clientcreditswindow.h"
 
 ClientCreditsWindow::ClientCreditsWindow()
 {
+    setWindowTitle("Управление кредитами");
+    setFixedWidth(300);
+    setFixedHeight(300);
 
+    QPushButton *takeLoan = new QPushButton("Взять кредит", this);
+
+    QGridLayout *gridLayout = new QGridLayout(this);
+
+    gridLayout->addWidget(takeLoan, 0, 0);
 }
