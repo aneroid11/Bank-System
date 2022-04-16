b4d28f934cde3c4bb90592bda3e0ebe0ecb984aa
aneroid11 <a-e-v2003@yandex.by>
2022-04-16 21:10:09 +0300

    dummy credits list

--- clientcreditswindow.cpp
+++ clientcreditswindow.cpp
@@ -1,6 +1,7 @@
 #include <QDebug>
 #include <QPushButton>
 #include <QGridLayout>
+#include <QLabel>
 
 #include "clientcreditswindow.h"
 
@@ -10,9 +11,19 @@ ClientCreditsWindow::ClientCreditsWindow()
     setFixedWidth(300);
     setFixedHeight(300);
 
+    QLabel *creditsListLabel = new QLabel("Список кредитов:", this);
+
+    creditsListWidget = new QListWidget(this);
+    creditsListWidget->addItem("02942049");
+
+    QPushButton *showInfo = new QPushButton("Информация о кредите", this);
     QPushButton *takeLoan = new QPushButton("Взять кредит", this);
+    QPushButton *makeMonthlyPayment = new QPushButton("Совершить платёж по кредиту", this);
 
     QGridLayout *gridLayout = new QGridLayout(this);
-
-    gridLayout->addWidget(takeLoan, 0, 0);
+    gridLayout->addWidget(creditsListLabel, 0, 0);
+    gridLayout->addWidget(creditsListWidget, 1, 0);
+    gridLayout->addWidget(showInfo, 2, 0);
+    gridLayout->addWidget(takeLoan, 3, 0);
+    gridLayout->addWidget(makeMonthlyPayment, 4, 0);
 }
--- clientcreditswindow.h
+++ clientcreditswindow.h
@@ -2,11 +2,15 @@
 #define CLIENTCREDITSWINDOW_H
 
 #include <QDialog>
+#include <QListWidget>
 
 class ClientCreditsWindow : public QDialog
 {
 public:
     ClientCreditsWindow();
+
+private:
+    QListWidget *creditsListWidget;
 };
 
 #endif // CLIENTCREDITSWINDOW_H
